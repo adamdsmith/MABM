a4e4a43d1b1b5d9da6aeec5f1d27d6497c32c8d9
Adam Smith <avianmigration@gmail.com>
2015-08-17 15:16:56 -0400

    Initial commit

---
+++ R/MABM_route.R
@@ -0,0 +1,205 @@
+#' Associate bat calls with simultaneously collected GPS information.  Creates multiple
+#' shapefiles and a *.csv for incorporation in Access database.
+#'
+#' This function associates bat calls classified with Bat Call Identification (BCID;
+#' \url{http::/www.batcallid.com}) software with simultaneously collected geographic
+#' location information.  For this application, bat calls were collected with (usually) an
+#' Anabat SD2 or the older Anabat SD1 (\url{https://www.titley-scientific.com}) and
+#' georeference by integrating the Anabat data logger, PDA and an external GPS.
+#'
+#'  If \code{for_import = TRUE}, the function will rearrange the columns to match the expected
+#'  import format of the MABM database and replace \code{NA}s with blanks to play nicely with
+#'  expected variable types.  This necessitates conversion of most (all?) columns to text
+#'  strings (which is not very handy if you want to work with the output in R).  This argument
+#'  is forced to \code{for_import = FALSE} if the user specifies \code{keep_output = TRUE}.
+#'
+#'  If \code{keep_output = TRUE}, the function creates a list containing (1) a \code{data.frame}
+#'  ('final_calls') containing information on the bat detections, (2) a
+#'  \code{\link[sp]{SpatialPointsDataFrame-class}} ('route_pt') of all GPS fixes along the route, (3) a
+#'  \code{\link[sp]{SpatialLinesDataFrame-class}} of these GPS fixes, and (4) a
+#'  \code{\link[sp]{SpatialPointsDataFrame-class}} ('call_pt') of all bat detections. This is handy if the
+#'  user wants to conduct further investigations in R. To access this output, remember to
+#'  assign the function output to an R object.
+#'
+#' @param for_import logical indicating whether the output *.csv file will be imported
+#'  into the MABM Access database (default = TRUE).  See details.
+#' @param keep_output logical (default = FALSE) that creates a list containing potentially
+#'  useful outputs.
+#' @importFrom magrittr "%>%"
+#' @import sp
+#' @importFrom rgdal writeOGR
+#' @export
+MABM_route <- function(for_import = TRUE, keep_output = FALSE) {
+
+    # If keeping output (keep_output = TRUE), override for_import
+    if (keep_output) for_import <- FALSE
+
+    ## Prompt user to specify route name (site_Name in Access)
+    # Load sites
+    #sites <- read.csv("../Data/site.list.csv")
+    sites <- read.csv(system.file("extdata", "site_list.csv", package = "MABM"), header = TRUE)
+    menu_items <- sort(paste0(sites$Site, ": (", sites$Location, ")"))
+    route_name <- tcltk::tk_select.list(menu_items, title="Choose the MABM route", multiple = FALSE)
+    # Drop the full location
+    route_name <- gsub(":.*$", "", route_name)
+
+    ## Retrieve GPS and BCID files
+    ## Assumes BCID file is in same directory
+    # GPS first
+    gps <- tcltk::tk_choose.files(default = "*.txt",
+                                  caption = "Select GPS text file.", multi = FALSE)
+
+    # Extract file input directory
+    trunc <- sapply(gregexpr("/", gps), tail, 1)
+    in_dir <- substr(gps, 1, trunc)
+
+    # Call file next
+    calls <- tcltk::tk_choose.files(default = paste0(in_dir, "*.xls"),
+                                    caption = "Select BCID output .xls file with bat call information.")
+
+    ## Initial GPS text file read and division into columns
+    # Get number of lines to skip at beginning of GPS files
+    # Looks for line that contains location headers
+    n_skip <- grep("Latitude", readr::read_lines(gps))
+
+    # Assign variable types and names; drop first and last columns
+    gps <- readr::read_fwf(gps, readr::fwf_empty(gps, skip = n_skip),
+                           col_types = "_ccncc_", skip = n_skip)
+    names(gps) <- c("lat", "lon", "alt_m", "date", "time")
+
+    # Restructure data
+#    gps <- gps %>% mutate(lat = as.numeric(substring(lat, 2)),
+#                          lon = ifelse(substring(lon, 1, 1) == "W",
+#                                       as.numeric(substring(lon, 2)) * -1,
+#                                       as.numeric(substring(lon, 2))),
+#                          date = lubridate::ymd(date),
+#                          dt = lubridate::ymd_hms(paste(date, time)),
+#                          call_id = gsub(":", "", time)) %>%
+#        arrange(dt) %>% # Sort chronologically
+#        mutate(order = 1:nrow(gps)) # Add order variable to facilitate QA/QC
+
+    # Use until new dplyr is released
+    gps <- plyr::ddply(gps, plyr::.(date), plyr::mutate, # Doesn't like dplyr's mutate
+                       lat = as.numeric(substring(lat, 2)),
+                       lon = ifelse(substring(lon, 1, 1) == "W",
+                                    as.numeric(substring(lon, 2)) * -1,
+                                    as.numeric(substring(lon, 2))),
+                       date = lubridate::ymd(date),
+                       dt = lubridate::ymd_hms(paste(date, time)),
+                       call_id = as.integer(gsub(":", "", time)))
+    gps <- dplyr::arrange(gps, dt)
+    gps$order <- 1:nrow(gps)
+
+    ## Call .xls file read and formatting
+    # Get start/end rows of relevant call information in BCID .xls file
+    # Looks at first sheet in .xls file
+    call_string <- do.call(paste0, readxl::read_excel(calls, sheet = 1, col_names = FALSE)) #concatenates all columns to simplify search
+    call_start <- grep("FILENAME", call_string) + 1 # Row after column headers; we have to make our own
+    # This may need some modification in future if format changes...
+    call_end <- grep("IDENTIFICATION", call_string) - 2 # Two rows before identification summary
+
+    # Read file
+    # This is all very hack-ish until readxl can incorporate the cellranger package
+    calls <- readxl::read_excel(calls, sheet = 1, col_names = FALSE)[call_start:call_end, 1:7]
+    names(calls) <- c("filename", "spp", "spp_perc", "group", "group_perc", "tot_pulses", "disc_prob")
+
+    # Get rid of some random tabs retained in the BCID .xls output
+    calls <- plyr::colwise(function(x) gsub("\t", "", x, fixed = TRUE))(calls)
+
+    # Replace blanks (i.e., "") with NAs
+    calls[calls == ""] <- NA
+
+    # Assign column types
+    set_col_types <- function(obj, types){
+        for (i in 1:length(obj)){
+            FUN <- switch(types[i],
+                          character = as.character,
+                          numeric = as.numeric,
+                          integer = as.integer)
+            obj[,i] <- FUN(obj[,i])
+            }
+        obj
+    }
+
+    calls <- set_col_types(calls, c(rep("character", 2), "numeric", "character", "numeric",
+                                  "integer", "numeric"))
+
+    # Restructure data
+    # transform filename to call ID
+    calls <- dplyr::mutate(calls,
+                           call_id = filename %>% stringr::str_sub(-8, -2) %>%
+                               gsub("[.]", "", .) %>%
+                                as.integer())
+
+    ## Associate (join) location data from GPS with bat calls
+    # However, rather than joining/merging, which require identical matches,
+    # we associate every call with the closest (absolute) GPS fix and record
+    # the difference in time (most will be 0; i.e., exact matches to the second)
+
+    # First, calculate difference in time (sec) between call and all GPS fixes
+    time_diffs <- outer(calls$call_id, gps$call_id, "-")
+    # Get index of the closest GPS fix
+    nearest_fix <- apply(time_diffs, 1, function(x) which.min(abs(x)))
+    gps_diff <- diag(time_diffs[, nearest_fix])
+
+    calls <- cbind(calls, dplyr::select(gps, -call_id)[nearest_fix, ], GPS_diff = gps_diff) %>%
+        dplyr::arrange(order) # Ensure ordered chronologically
+
+    ## Rearranges columns for import into MABM MS Access database (column order
+    ## specified in import specification file 'calls1'
+    if (for_import) {
+        import <- with(calls,
+                     data.frame(lat, lon, alt_m, date, time, call_id, filename,
+                                spp, spp_perc, group, group_perc, tot_pulses, disc_prob,
+                                call_id, order, gps_diff, site_name = route_name))
+        # Replace NAs with blanks to play nicely with conversion in Access
+        # To do so requires the conversion of all fields to text
+        # NOTE: `time` is the time of the GPS fix, not the detection
+        import <- sapply(import, as.character)
+        import[is.na(import)] <- ""
+    }
+
+    ## Create output directory
+    route_date <- min(gps$date) %>% as.character() %>% gsub("-", "", .)
+    out_name <- paste(route_name, route_date, sep = "_")
+    out_dir <- paste0(in_dir, out_name)
+    dir.create(out_dir)
+
+    # Write final bat call data file with associated GPS information
+    name <- paste0("Calls_", out_name, "_final.csv")
+    write.csv(import, file = paste(out_dir, name, sep = "/"), quote = FALSE)
+
+    ## Create point shapefile of all GPS locations (SavedRoute)
+    gps_spdf <- gps
+    coordinates(gps_spdf) <- ~ lon + lat
+    proj4string(gps_spdf) <- CRS("+proj=longlat +datum=WGS84")
+    name <- paste0("SavedRoute_", out_name)
+    writeOGR(gps_spdf, out_dir, name, driver = "ESRI Shapefile", overwrite_layer = TRUE)
+
+    ## Create line shapefile of the route (RouteLine)
+    gps_sldf <- Line(coordinates(gps_spdf)) %>% list() %>% Lines(., ID = "route") %>%
+        list() %>% SpatialLines()
+    df <- data.frame(id = "route")
+    rownames(df) <- "route"
+    gps_sldf <- SpatialLinesDataFrame(gps_sldf, df)
+    proj4string(gps_sldf) <- proj4string(gps_spdf)
+    name <- paste0("RouteLine_", out_name)
+    writeOGR(gps_sldf, out_dir, name, driver = "ESRI Shapefile", overwrite_layer = TRUE)
+
+    ## Create point shapefile of locations with bat calls (Calls)
+    calls_spdf <- calls
+    coordinates(calls_spdf) <- ~ lon + lat
+    proj4string(calls_spdf) <- proj4string(gps_spdf)
+    name <- paste0("Calls_", out_name)
+    writeOGR(calls_spdf, out_dir, name, driver = "ESRI Shapefile", overwrite_layer = TRUE)
+
+    cat(paste0("The folder '", out_name, "' has been created in\n'",
+               in_dir, "'\n", "and contains the following:\n\n",
+               "Shapefiles:\n",
+               paste(list.files(path = out_dir, pattern=".shp$"), collapse = "\n"),
+               "\n\nText files:\n",
+               list.files(path = out_dir, pattern=".csv$")))
+
+    if (keep_output) return(list(final_calls = calls, route_pt = gps_spdf,
+                                 route_line = gps_sldf, call_pt = calls_spdf))
+}
---
+++ R/plot_MABM_route.R
@@ -0,0 +1,235 @@
+#' Plot MABM route and, if requested, bat calls detected along the route.
+#'
+#' This function plots a MABM route (from a shapefile created with \code{\link{MABM_route}})
+#' and, if requested (with the \code{bats} argument), bat calls (by species identification)
+#' detected along the route. To work properly this function requires an internet connection.
+#'
+#' With the \code{bats} argument, the user can specify whether all detected calls should be
+#' displayed or only those that meet (or do not meet) a user-specified criterion for a "good"
+#' georeferenced call. This criterion is specified as the number of seconds between a detected
+#' call and the closest (in time) GPS fix.
+#'
+#' Species classifications can be turned off using \code{spp = FALSE}.
+#'
+#' By default, every third bat detection (when plotted) is labeled with its associated time
+#' stamp and the route is shaded by time (minutes) elapsed from the first GPS fix to help
+#' visualize the route.  These can be suppressed with \code{labels = FALSE}.
+#'
+#' @param type character string providing map theme. Reasonable choices include "terrain",
+#'  "terrain-background", "satellite", "roadmap" (the default), and "hybrid" (google maps).
+#' @param spp logical indicating whether detections should be categorized by species;
+#'  defaults to TRUE.
+#' @param bats character string indicating which, if any, bat calls to display along the
+#'  MABM route.  Options available include "all" (default), "good", "bad", and "none".
+#'  The distinction between "good" and "bad" calls depends on the user-specified
+#'  \code{bad_gps} argument.
+#' @param labels logical (default = TRUE) indicating whether every third detection
+#'  should be labeled with a time stamp to assist in visualizing the route. Ignored if the
+#'  user opts not to add detections to the route.
+#' @param bad_gps integer indicating the absolute maximum number of seconds between a
+#'  detected bat call and the nearest (in time) GPS location fix the user will consider as
+#'  a "good" (usually interpreted "usable") georeference for a given bat call.  All calls
+#'  greater than this value will be recorded as having a "bad" georeference. The actual
+#'  time difference between a bat detection and the closest GPS location is noted in the
+#'  output under the column heading 'GPS_diff'; positive (negative) values indicate the
+#'  detection occurred after (before) the associated GPS location.
+#' @return a ggmap object (a classed raster object with a bounding box attribute)
+#' @import ggplot2
+#' @import ggmap
+#' @importFrom magrittr "%>%"
+#' @export
+
+plot_MABM_route <- function(type = "roadmap", spp = TRUE,
+                            bats = c("all", "good", "bad", "none"),
+                            labels = TRUE, bad_gps = 5) {
+
+    bats <- match.arg(bats)
+    add_calls <- bats %in% c("all", "good", "bad")
+
+    ## Set some graphical parameters
+    # Setting theme for producing figures
+    theme_set(theme_bw(base_size = 16))
+    theme_update(panel.grid.minor = element_blank(),
+                 panel.grid.major= element_blank(),
+                 panel.border = element_blank(),
+                 panel.background= element_blank(),
+                 axis.line = element_line(color = "black"))
+
+    #Create a custom color scale to consistently display species, if requested
+    bat_fills <- c("orange3", "orange3", "sienna", "red2", "forestgreen", "forestgreen",
+        "gray40", "gray40", "gray40", "gray40", "gray40", "gray40", "royalblue4",
+        "gold", "white")
+    names(bat_fills) <- c("CORA", "COTO", "EPFU", "LABO", "LACI", "LANO", "MYAU",
+                          "MYGR", "MYLE", "MYLU", "MYSE", "MYSO", "NYHU", "PESU",
+                          "UNKN")
+    bat_fillScale <- scale_fill_manual(name = "Species", values = bat_fills,
+                                       guide = guide_legend(override.aes=list(shape=22)))
+
+    ## Pull in GPS route (SavedRoute)
+    gps <- tcltk::tk_choose.files(default = "*.shp",
+                           caption = "Select GPS shapefile (e.g., 'SavedRoute.shp).")
+
+    ## Pull in call file, if requested
+    if(add_calls) {
+        # Extract file input directory
+        trunc <- sapply(gregexpr("/", gps), tail, 1)
+        in_dir <- substr(gps, 1, trunc)
+
+        calls <- tcltk::tk_choose.files(default = paste0(in_dir, "*.shp"),
+                                 caption = "Select bat call shapefile (e.g., 'Calls.shp').")
+    }
+
+    # Split GPS file name
+    path_gps <- gps %>% strsplit(., "/") %>% unlist() %>% head(., -1) %>% paste(., collapse = "/")
+    shape_gps <- gps %>% strsplit(., "/") %>% unlist() %>% tail(., 1) %>% tools::file_path_sans_ext()
+
+    # Split call file name
+    if (add_calls) {
+        path_calls <- calls %>% strsplit(., "/") %>% unlist() %>% head(., -1) %>% paste(., collapse = "/")
+        shape_calls <- calls %>% strsplit(., "/") %>% unlist() %>% tail(., 1) %>% tools::file_path_sans_ext()
+    }
+
+    # Read shapefile(s)
+    gps <- rgdal::readOGR(path_gps, shape_gps, verbose = FALSE, stringsAsFactors = FALSE)
+    if (add_calls) calls <- rgdal::readOGR(path_calls, shape_calls, verbose = FALSE, stringsAsFactors = FALSE)
+
+    # Make data frame for plotting GPS route data
+    plot_dat <- data.frame(gps@coords, gps@data)
+    names(plot_dat)[1:2] <- c("lon", "lat") # rename coordinate fields
+
+    # Add elapsed time
+    plot_dat <- plyr::ddply(plot_dat, plyr::.(date), plyr::mutate,
+                  t_seg = c(0, diff(lubridate::decimal_date(lubridate::ymd_hms(dt)))) * 525960, # elapsed time since last fix
+                  t_elapsed = round(cumsum(t_seg), 1))
+
+    ## Background map
+    # Set bounding box of route for auto zoom calculation
+    # May not always work perfectly but not too bad
+    bbox <- make_bbox(lon, lat, plot_dat, f = 0.25)
+
+    bg <- get_map(location = bbox, maptype=type, scale=2)
+    bg <- ggmap(bg, extent="panel")
+
+    p <- bg
+
+    ## Add complete route to background map
+    # We map the route using points so breaks in the GPS log are more apparent
+    if (labels) {
+        p <- p +
+            geom_point(data = plot_dat, aes(x = lon, y = lat, colour = t_elapsed),
+                       size = 2) +
+            scale_colour_gradient("Elapsed time (minutes)", low = "#2166ac", high = "#b2182b",
+                                  guide = guide_colorbar(direction = "horizontal",
+                                                         title.position = "top"))
+    } else {
+        p <- p + geom_point(data = plot_dat, aes(x = lon, y = lat))
+    }
+
+    # Change axis labels and move legends
+    p <- p + xlab("Longitude") + ylab("Latitude") +
+        theme(legend.position = "top")
+
+    ## If requested, add bat calls to the route
+    # First, create data frame for plotting
+    if (add_calls) {
+        bat_dat <- data.frame(calls@coords, calls@data)
+        names(bat_dat)[1:2] <- c("lon", "lat") # rename coordinate fields
+
+        good_bats <- subset(bat_dat, abs(GPS_diff) <= bad_gps)
+        bad_bats <- subset(bat_dat, abs(GPS_diff) > bad_gps)
+
+        if (nrow(bad_bats) == 0) {
+            cat(paste0("\nAll bat locations meet your criterion for a 'good' fix (GPS fix within ", bad_gps, " sec).\n"))
+            cat("Showing all detected bat calls.\n\n")
+            bats <- "good"
+        }
+    }
+
+    if (bats == "bad") {
+
+        # Add calls to plot
+        if (spp) {
+            p <- p +
+                # Only points with "bad" GPS fixes
+                geom_point(data = bad_bats, aes(fill = spp),
+                           shape = 23, size = 8) +
+                bat_fillScale
+        } else {
+            p <- p +
+                geom_point(data = bad_bats,
+                           shape = 23, size = 8)
+        }
+
+        p <- p +
+            # Label "bad" GPS fixes with time difference
+            geom_text(data = bad_bats,
+                      aes(label = GPS_diff), size = 4)
+
+        cat(paste0("\n", nrow(bad_bats), " bat call(s) meet your bad GPS fix criterion (>", bad_gps, " sec from closest GPS fix).\n\n"))
+        print(bad_bats[, c("filename", "lat", "lon", "spp", "date", "time",
+                           "call_id", "order", "GPS_diff")])
+    }
+
+    if (bats == "all") {
+
+        # Label every 3rd call to avoid messy labels
+        which_to_lab <- seq(1, nrow(bat_dat), 3)
+        labels_dat <- bat_dat[which_to_lab, ]
+
+        all_bats <- rbind(cbind(good_bats, bad = 0),
+                          cbind(bad_bats, bad = 1))
+
+        # Add calls to plot
+        if (spp) {
+            p <- p +
+                geom_point(data = all_bats, aes(shape = as.factor(bad), fill = spp),
+                           size = 8) +
+                scale_shape_manual("", values = c(21, 23), guide = "none") +
+                bat_fillScale
+        } else {
+            p <- p +
+                geom_point(data = all_bats, aes(shape = as.factor(bad)), size = 8) +
+                scale_shape_manual("", values = c(21, 23))
+        }
+
+        p <- p +
+            # Label "bad" GPS fixes with time difference
+            geom_text(data = subset(bat_dat, abs(GPS_diff) > bad_gps),
+                      aes(label = GPS_diff), size = 4)
+
+        if (labels) {
+            # Label every 3rd point with time stamp
+            p <- p + geom_text(data = labels_dat, aes(label = time), hjust = 0)
+        }
+    }
+
+    if (bats == "good") {
+
+        # Label every 3rd call to avoid messy labels
+        which_to_lab <- seq(1, nrow(good_bats), 3)
+        labels_dat <- good_bats[which_to_lab, ]
+
+        # Add calls to plot
+        if (spp) {
+            p <- p +
+                # Only points with "bad" GPS fixes
+                geom_point(data = good_bats, aes(fill = spp),
+                           shape = 21, size = 8) +
+                bat_fillScale
+        } else {
+            p <- p +
+                geom_point(data = good_bats,
+                           shape = 21, size = 8)
+        }
+
+        # Add labels, if requested
+        if (labels) {
+            p <- p +
+                # Label every 3rd point with time stamp
+                geom_text(data = labels_dat, aes(label = time), hjust = 0)
+        }
+    }
+
+    return(p)
+
+}
